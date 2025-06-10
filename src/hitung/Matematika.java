2255d8b0236d9538d1292782a578f2083ce0745f
VernandaZF <vernandamlz@gmail.com>
2025-06-10 19:33:03 +0800

    Membuat constructor dan setPenjumlahan

---
+++ src/hitung/Matematika.java
@@ -0,0 +1,17 @@
+package hitung;
+
+public class Matematika {
+    private double bil1;
+    private double bil2;
+
+    // Constructor untuk mengisi nilai bil1 dan bil2
+    public Matematika(double bil1, double bil2) {
+        this.bil1 = bil1;
+        this.bil2 = bil2;
+    }
+
+    // Method untuk mengembalikan hasil penjumlahan bil1 + bil2
+    public double setPenjumlahan() {
+        return bil1 + bil2;
+    }
+}
