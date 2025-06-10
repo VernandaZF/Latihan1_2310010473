575ca9711fd4e3863a51a65c33754aee5e71b3d5
VernandaZF <vernandamlz@gmail.com>
2025-06-10 19:42:33 +0800

    Membuat class MatematikaBeraksi

--- src/hitung/MatematikaBeraksi.java
+++ src/hitung/MatematikaBeraksi.java
@@ -1,14 +1,16 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package hitung;
 
-/**
- *
- * @author Sohib
- */
+
 public class MatematikaBeraksi {
-    
+    public static void main(String[] args) {
+        
+        Matematika vernanda = new Matematika(7, 3);
+        
+        // Menampilkan hasil operasi matematika
+        System.out.println("Penjumlahan: " + vernanda.setPenjumlahan());
+        System.out.println("Pengurangan: " + vernanda.setPengurangan());
+        System.out.println("Perkalian: " + vernanda.setPerkalian());
+        System.out.println("Pembagian: " + vernanda.setPembagian());
+    }
 }
