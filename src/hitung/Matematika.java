95db4a69a6641c57787c3c48b93fd739724c7f8d
VernandaZF <vernandamlz@gmail.com>
2025-06-10 19:35:56 +0800

    Melengkapi method matematika

--- src/hitung/Matematika.java
+++ src/hitung/Matematika.java
@@ -4,14 +4,27 @@ public class Matematika {
     private double bil1;
     private double bil2;
 
-    // Constructor untuk mengisi nilai bil1 dan bil2
     public Matematika(double bil1, double bil2) {
         this.bil1 = bil1;
         this.bil2 = bil2;
     }
 
-    // Method untuk mengembalikan hasil penjumlahan bil1 + bil2
     public double setPenjumlahan() {
         return bil1 + bil2;
+    }
+
+    // Method untuk pengurangan
+    public double setPengurangan() {
+        return bil1 - bil2;
+    }
+
+    // Method untuk perkalian
+    public double setPerkalian() {
+        return bil1 * bil2;
+    }
+
+    // Method untuk pembagian
+    public double setPembagian() {
+        return bil1 / bil2;  // Mengembalikan Infinity jika pembagi 0
     }
 }
